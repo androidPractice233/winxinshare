b8087bcee5213e09817868efaeff17d6e29d7e9c
huangtiansheng <873966702@qq.com>
2018-05-10 12:30:43 +0800

    Rejected commit: Default

--- app/src/main/java/com/scut/weixinshare/view/fragment/MomentDetailFragment.java
+++ app/src/main/java/com/scut/weixinshare/view/fragment/MomentDetailFragment.java
@@ -26,6 +26,7 @@ import com.scut.weixinshare.model.Moment;
 import com.scut.weixinshare.utils.KeyBroadUtils;
 import com.scut.weixinshare.utils.ToastUtils;
 import com.scut.weixinshare.view.BigPicActivity;
+import com.scut.weixinshare.view.PersonalMomentActivity;
 import com.scut.weixinshare.view.UserActivity;
 import com.scut.weixinshare.view.LoginActivity;
 import com.scut.weixinshare.view.component.CommentView;
@@ -132,7 +133,7 @@ public class MomentDetailFragment extends Fragment implements MomentDetailContra
 
     @Override
     public void showUserDataUI(String userId) {
-        Intent intent=new Intent(getContext(),UserActivity.class);
+        Intent intent=new Intent(getContext(),PersonalMomentActivity.class);
         intent.putExtra("userId",userId);
         startActivity(intent);
     }
